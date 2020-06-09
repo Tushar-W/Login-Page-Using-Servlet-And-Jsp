3ea5fa8fdd2efe506d1aa6124cc1e79e29be6bba
Tushar-W <twankhede95@gmail.com>
2020-06-08 18:24:15 -0700

    ADDED:code to create Login servlet page

--- src/main/java/com/servlet/LoginServlet.java
+++ src/main/java/com/servlet/LoginServlet.java
@@ -1,6 +1,8 @@
 package com.servlet;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,12 +10,32 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "LoginServlet")
+@WebServlet(
+        description = "Login Servlet Testing",
+        urlPatterns = {"/com.servlet.LoginServlet"},
+        initParams = {
+                @WebInitParam(name = "username", value = "Tushar"),
+                @WebInitParam(name = "password", value = "Tushar@95")
+        }
+)
 public class LoginServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h3>Hello World Tushar My First Servlet!</h3>");
-        out.close();
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //get request parameters for userID and password
+        String user = req.getParameter("user");
+        String pwd = req.getParameter("pwd");
+        //get servlet config init params
+        String userID = getServletConfig().getInitParameter("username");
+        String password = getServletConfig().getInitParameter("password");
+        if (userID.equals(user) && password.equals(pwd)) {
+            req.setAttribute("user", user);
+            req.getRequestDispatcher("LoginSuccess.jsp").forward(req, resp);
+        }else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = resp.getWriter();
+            out.println("<font color=red>Either user name or password is wrong_</font>");
+            rd.include(req, resp);
+        }
+
     }
 }
